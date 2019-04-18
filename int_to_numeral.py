e1d3771796ce51bd0cea6511832c25f0b84bc3a8
msilvavieira <not@t.all>
2019-04-18 14:39:31 -0300

    Prototype Int-to-Numeral algorithm, base tests

--- int_to_numeral.py
+++ int_to_numeral.py
@@ -1,17 +1,88 @@
-numerals = {
-    0: "zero", 1: "um", 2: "dois", 3: "três", 4: "quatro", 
-    5: "cinco", 6: "seis", 7: "sete", 8: "oito", 9: "nove", 
+numerals = (
+    ("zero", "um", "dois", "três", "quatro", 
+     "cinco", "seis", "sete", "oito", "nove",
+     "dez", "onze", "doze", "treze", "quatorze", 
+     "quinze", "dezesseis", "dezessete", "dezoito", "dezenove",),
 
-    10: "dez", 11: "onze", 12: "doze", 13: "treze", 14: "quatorze", 
-    15: "quinze", 16: "dezesseis", 17: "dezessete", 18: "dezoito", 19: "dezenove", 
+    ("", "", "vinte", "trinta", "quarenta", "cinquenta",
+     "sessenta", "setenta", "oitenta", "noventa",),
 
-    20: "vinte", 30: "trinta", 40: "quarenta", 50: "cinquenta", 60: "sessenta", 
-    70: "setenta", 80: "oitenta", 90: "noventa",
+    ("", "cento", "duzentos", "trezentos", "quatrocentos",
+     "quinhentos", "seiscentos", "setecentos", "oitocentos",
+     "novecentos",),
 
-    100: "cem", 200: "duzentos", 300: "trezentos", 400: "quatrocentos",
-    500: "quinhentos", 600: "seiscentos", 700: "setecentos", 800: "oitocentos",
-    900: "novecentos",
+    #("mil",),
+)
 
-    1000: "mil"
-}
+
+
+
+def int_to_numeral(integer):
+
+    numeral = ''
+
+    def parse_tens(integer):
+        numeral = ''
+        if integer < 20:
+            numeral =  numerals[0][integer]
+        else:
+            div = divmod(integer, 10)
+            numeral = numerals[1][div[0]]
+            if div[1] != 0:
+                numeral += ' e '
+                numeral += numerals[0][div[1]]
+        return numeral
+
+    def parse_hundreds(integer):
+        numeral = ''
+        if integer == 0:
+            pass
+        elif integer < 100:
+            numeral = parse_tens(integer)
+        elif integer == 100:
+            numeral = 'cem'
+        elif integer % 100 == 0:
+            numeral = numerals[1][integer / 100]
+        else:
+            div = divmod(integer, 100)
+            numeral = numerals[2][div[0]]
+            if numeral:
+                numeral += ' e '
+            numeral += parse_tens(div[1])
+        return numeral
+
+    def parse_thousands(integer):
+        numeral = ''
+        if integer == 1000:
+            numeral = 'mil'
+        else:
+            div = divmod(integer, 1000)
+            if div[0] == 1:
+                numeral = 'mil '
+            elif div[0] < 20:
+                numeral = parse_tens(div[0])
+                numeral += ' mil '
+            else:
+                numeral = parse_hundreds(div[0])
+                numeral += ' mil '
+            if div[1] < 100 and div[1] > 0:
+                numeral += 'e '
+            numeral += parse_hundreds(div[1])
+        return numeral
+
+    minus = False
+    if integer < 0:
+        integer = abs(integer)
+        minus = True
+    if integer < 100:
+        numeral = parse_tens(integer)
+    elif integer < 1000:
+        numeral = parse_hundreds(integer)
+    else:
+        numeral = parse_thousands(integer)
+
+    numeral = numeral.strip()
+    if minus:
+        numeral = 'menos ' + numeral
+    return numeral
 
